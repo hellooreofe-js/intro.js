7d693f9ec9b949d4299b7120ac2dd3cdcc337462
hellooreofe-js <oreofeade09@gmail.com>
2026-01-17 19:53:36 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,9 @@
+const name = "Adeoti Oreofeoluwa Joseph"
+const age = 16
+const favoritelanguage = "JavaScript"
+
+console.log ("Name:", name)
+console.log ("Age:", age)
+console.log ("Favorite Programming Language:", favoritelanguage)
+
+
